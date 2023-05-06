05f055179e7e9452aab06d8350d19d8a59a94113
Dennis Magnusson <magnden@gmail.com>
2023-05-06 13:40:27 +0300

    rename and clear foods screen

---
+++ screens/FoodsScreen.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {
+  SafeAreaView,
+  Text
+} from "react-native";
+
+const FoodsScreen = ({ navigation }) => {
+
+  return (
+    <SafeAreaView>
+      <Text>Foods screen</Text>
+    </SafeAreaView>
+  );
+};
+
+export default FoodsScreen;
