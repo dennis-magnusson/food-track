f1f4d7d7a56fda677e240e5501e64da5afd052d1
Dennis Magnusson <magnden@gmail.com>
2023-05-06 13:38:36 +0300

    add searching to meal screen

--- screens/MealScreen.js
+++ screens/MealScreen.js
@@ -1,13 +1,100 @@
-import { SafeAreaView, Text } from "react-native";
+import React, { useState } from "react";
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View
+} from "react-native";
 
-const MealScreen = ({ route }) => {
-  // const { mealData } = route.params;
+const MealScreen = ({ navigation, route }) => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  navigation.setOptions({ title: route.params.mealData.name})
+
+  const searchFoods = async () => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(
+        `https://world.openfoodfacts.org/cgi/search.pl?search_terms=${searchQuery}&page_size=20&json=true`
+      );
+      const data = await response.json();
+      navigation.navigate("Search Results", { results: data.products, isLoading, searchQuery }); // navigate to the SearchResults screen and pass the search results as a parameter
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const handleAddFood = () => {
+    console.log("Add food");
+  };
 
   return (
-    <SafeAreaView>
-      <Text> meal screen </Text>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        <TouchableOpacity onPress={handleAddFood}>
+          <Text style={styles.addFoodButton}>+ Add Custom Food</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.searchBar}>
+        <TextInput
+          style={styles.searchInput}
+          placeholder="Search for a food"
+          value={searchQuery}
+          onChangeText={setSearchQuery}
+          onSubmitEditing={searchFoods}
+        />
+        <TouchableOpacity style={styles.searchButton} onPress={searchFoods}>
+          <Text style={styles.searchButtonText}>Search</Text>
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  header: {
+    backgroundColor: "#f0f0f0",
+    padding: 20,
+    alignItems: "flex-end",
+  },
+  addFoodButton: {
+    color: "blue",
+    fontWeight: "bold",
+  },
+  searchBar: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#f0f0f0",
+    padding: 10,
+    margin: 10,
+    borderRadius: 5,
+  },
+  searchInput: {
+    flex: 1,
+    height: 40,
+    backgroundColor: "#fff",
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    marginRight: 10,
+  },
+  searchButton: {
+    backgroundColor: "blue",
+    padding: 10,
+    borderRadius: 5,
+  },
+  searchButtonText: {
+    color: "#fff",
+    fontWeight: "bold",
+  },
+});
 
 export default MealScreen;
