86340cd2db9e56907e69e3fe21d40a7ea420d8e6
Dennis Magnusson <magnden@gmail.com>
2023-07-22 12:39:52 +0300

    modify screen to list food library

--- screens/AddEntryScreen.tsx
+++ screens/AddEntryScreen.tsx
@@ -1,21 +1,38 @@
 import { useNavigation } from "@react-navigation/native";
-import React, { useContext, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { ScrollView, StyleSheet, View } from "react-native";
 import FoodsList from "../components/FoodsList";
 import MyButton from "../components/MyButton";
 import MySafeAreaView from "../components/MySafeAreaView";
 import SearchFood from "../components/SearchFood";
-import { DayContext } from "../context/AppContext";
+import { fetchAllFoods } from "../services/databaseService";
 import { colors } from "../theme";
-import { MealType } from "../types";
+import { Food, MealType } from "../types";
 
 const AddEntryScreen = ({ route }) => {
   const navigation = useNavigation();
   const [searchQuery, setSearchQuery] = useState<string>("");
   const [mealType, setMealType] = useState<MealType>(route.params.meal.type);
-  const day = useContext(DayContext);
+  const [foods, setFoods] = useState<Food[]>([]);
+  const [filteredFoods, setFilteredFoods] = useState<Food[]>(foods);
 
-  navigation.setOptions({ title: `Add food to ${mealType}` });
+  useEffect(() => {
+    fetchAllFoods((f) => {
+      setFoods(f);
+      setFilteredFoods(f);
+    });
+  }, []);
+
+  const searchFoods = (query: string) => {
+    setSearchQuery(query);
+    setFilteredFoods(
+      foods.filter((food) =>
+        food.name.toLowerCase().includes(searchQuery.toLowerCase())
+      )
+    );
+  };
+
+  navigation.setOptions({ title: `Add food - ${mealType}` });
 
   const handleAddCustomFood = () => {
     // navigation.navigate("Custom Food");
@@ -24,19 +41,19 @@ const AddEntryScreen = ({ route }) => {
   return (
     <MySafeAreaView>
       <View>
-        <ScrollView contentContainerStyle={styles.containerInner}>
-          <FoodsList foods={day.meals[mealType]} />
-        </ScrollView>
         <SearchFood
           searchQuery={searchQuery}
           setSearchQuery={setSearchQuery}
-          searchFoods={undefined}
+          searchFoods={searchFoods}
         />
         <MyButton
           text="+ Add Custom Food"
           style={styles.addButton}
           onPress={handleAddCustomFood}
         />
+        <ScrollView contentContainerStyle={styles.containerInner}>
+          <FoodsList foods={filteredFoods} />
+        </ScrollView>
       </View>
     </MySafeAreaView>
   );
@@ -44,7 +61,6 @@ const AddEntryScreen = ({ route }) => {
 
 const styles = StyleSheet.create({
   containerInner: {
-    paddingTop: 16,
     backgroundColor: colors.lightBackground,
   },
   addButton: {
