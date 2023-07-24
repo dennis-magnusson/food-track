adcc0ba0d01c3f3367eface25c8949b42df6f824
Dennis Magnusson <magnden@gmail.com>
2023-07-24 20:58:30 +0300

    custom screen to the today stack

--- navigation/TodayStack.tsx
+++ navigation/TodayStack.tsx
@@ -1,5 +1,6 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import AddEntryScreen from "../screens/AddEntryScreen";
+import CustomFoodScreen from "../screens/CustomFoodScreen";
 import TodayScreen from "../screens/TodayScreen";
 
 const TodayStack = (): JSX.Element => {
@@ -9,6 +10,7 @@ const TodayStack = (): JSX.Element => {
     <Stack.Navigator initialRouteName="Today">
       <Stack.Screen name="Today" component={TodayScreen} />
       <Stack.Screen name="Add Food" component={AddEntryScreen} />
+      <Stack.Screen name="Add Custom Food" component={CustomFoodScreen} />
     </Stack.Navigator>
   );
 };
