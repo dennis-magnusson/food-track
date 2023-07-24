d270ce497e6b23bd3ce1942d6a1ff84c833881d0
Dennis Magnusson <magnden@gmail.com>
2023-07-24 20:09:32 +0300

    change import to stay consistent

--- screens/TodayScreen.tsx
+++ screens/TodayScreen.tsx
@@ -1,4 +1,4 @@
-import { useNavigation } from "@react-navigation/core";
+import { useNavigation } from "@react-navigation/native";
 import React from "react";
 import { ScrollView, StyleSheet } from "react-native";
 import DailyTotals from "../components/DailyTotals";
