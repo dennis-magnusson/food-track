ac27722dcc7d5062b9d72356d76cfecaedcc9f63
Dennis Magnusson <magnden@gmail.com>
2023-12-02 19:54:06 +0200

    implement changing the day

--- screens/Main/MainScreen.tsx
+++ screens/Main/MainScreen.tsx
@@ -1,22 +1,25 @@
 import { useNavigation } from "@react-navigation/native";
-import React from "react";
+import React, { useContext } from "react";
 import { ScrollView, StyleSheet } from "react-native";
+import { DayDispatchContext } from "../../context/AppContext";
 import MySafeAreaView from "../../shared/MySafeAreaView";
 import { colors } from "../../theme";
 import { MainScreenNavigationProp, MealType } from "../../types";
+import { loadDayData } from "../../utils/loadDayData";
 import DailyTotals from "./DailyTotals";
 import DateSelector from "./DateSelector";
 import MealsList from "./MealsList";
 
 const MainScreen: React.FC = (): JSX.Element => {
   const navigation = useNavigation<MainScreenNavigationProp>();
+  const dispatch = useContext(DayDispatchContext);
 
   const handleMealPress = (mealType: MealType): void => {
     navigation.navigate("Meal", { mealType });
   };
 
   const changeDay = (newDay: Date) => {
-    console.log(newDay);
+    loadDayData(dispatch, newDay);
   };
 
   return (
