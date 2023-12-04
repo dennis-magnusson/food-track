a88f728a53336a984cc6129abcf6af81496ed639
Dennis Magnusson <magnden@gmail.com>
2023-12-04 21:56:37 +0200

    change sql table names to be lower case

--- services/sql.ts
+++ services/sql.ts
@@ -1,5 +1,5 @@
 export const CREATE_TABLE_FOODS = `
-  CREATE TABLE IF NOT EXISTS Food (
+  CREATE TABLE IF NOT EXISTS food (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL,
     calories REAL NOT NULL,
@@ -14,7 +14,7 @@ export const CREATE_TABLE_FOODS = `
 `;
 
 export const CREATE_TABLE_MEALS = `
-  CREATE TABLE IF NOT EXISTS Meal (
+  CREATE TABLE IF NOT EXISTS meal (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     date TEXT NOT NULL,
     type TEXT CHECK(type IN ('breakfast', 'lunch', 'dinner', 'snack')) NOT NULL,
@@ -23,66 +23,66 @@ export const CREATE_TABLE_MEALS = `
 `;
 
 export const CREATE_TABLE_MEAL_FOODS = `
-  CREATE TABLE IF NOT EXISTS MealFood (
+  CREATE TABLE IF NOT EXISTS mealfood (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     meal_id INTEGER NOT NULL,
     food_id INTEGER NOT NULL,
     amount REAL NOT NULL,
-    FOREIGN KEY (meal_id) REFERENCES Meal(id),
-    FOREIGN KEY (food_id) REFERENCES Food(id)
+    FOREIGN KEY (meal_id) REFERENCES meal(id),
+    FOREIGN KEY (food_id) REFERENCES food(id)
   )
 `;
 
 export const INSERT_FOOD = `
-  INSERT INTO Food (name, calories, protein, carbs, sugar, fiber, fat, salt, per100unit)
+  INSERT INTO food (name, calories, protein, carbs, sugar, fiber, fat, salt, per100unit)
   VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
 `;
 
 export const INSERT_MEAL = `
-  INSERT INTO Meal (date, type) VALUES (?, ?)
+  INSERT INTO meal (date, type) VALUES (?, ?)
 `;
 
 export const INSERT_FOOD_TO_MEAL = `
-    INSERT INTO MealFood (meal_id, food_id, amount)
+    INSERT INTO mealfood (meal_id, food_id, amount)
     VALUES (?, ?, ?)
 `;
 
 export const FETCH_ALL_FOODS = `
-  SELECT * FROM Food
+  SELECT * FROM food
 `;
 
 export const INSERT_OR_IGNORE_MEAL = `
-  INSERT OR IGNORE INTO Meal (date, type) VALUES (?, ?)
+  INSERT OR IGNORE INTO meal (date, type) VALUES (?, ?)
 `;
 
 export const UPDATE_AMOUNT_FOOD_ENTRY = `
-    UPDATE MealFood SET amount = ? WHERE id = ?
+    UPDATE mealfood SET amount = ? WHERE id = ?
 `;
 
 export const DELETE_FOOD_ENTRY = `
-    DELETE FROM MealFood WHERE id = ?;
+    DELETE FROM mealfood WHERE id = ?;
 `;
 
 export const FETCH_MEALS_WITH_FOODS_BY_DATE = `
   SELECT 
-    Meal.id AS meal_id, 
-    Meal.date, 
-    Meal.type, 
-    MealFood.food_id,
-    MealFood.id AS entry_id,
-    Food.name,
-    Food.calories,
-    Food.protein,
-    Food.carbs,
-    Food.sugar,
-    Food.fiber,
-    Food.fat,
-    Food.salt,
-    Food.per100unit,
-    MealFood.amount
-  FROM Meal 
-  LEFT JOIN MealFood ON Meal.id = MealFood.meal_id
-  LEFT JOIN Food ON MealFood.food_id = Food.id
-  WHERE Meal.date = ?
-  ORDER BY Meal.type, Food.name
+    meal.id AS meal_id, 
+    meal.date, 
+    meal.type, 
+    mealfood.food_id,
+    mealfood.id AS entry_id,
+    food.name,
+    food.calories,
+    food.protein,
+    food.carbs,
+    food.sugar,
+    food.fiber,
+    food.fat,
+    food.salt,
+    food.per100unit,
+    mealfood.amount
+  FROM meal 
+  LEFT JOIN mealfood ON meal.id = mealfood.meal_id
+  LEFT JOIN food ON mealfood.food_id = food.id
+  WHERE meal.date = ?
+  ORDER BY meal.type, food.name
 `;
