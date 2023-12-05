25c042255241b95085fc92965632bee07a1e8b80
Dennis Magnusson <magnden@gmail.com>
2023-12-05 23:10:33 +0200

    add type for setting stack param list

--- types.ts
+++ types.ts
@@ -56,6 +56,10 @@ export type RootStackParamList = {
   };
 };
 
+export type SettingsStackParamList = {
+  MainSettings: undefined;
+};
+
 export interface RawMealDataRow {
   amount: number | null;
   calories: number | null;
