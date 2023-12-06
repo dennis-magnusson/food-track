02c2dcb8f1004de3c46d249929b25ba2eb41d2fd
Dennis Magnusson <magnden@gmail.com>
2023-12-06 16:01:16 +0200

    add basic styles for skeleton

--- screens/Settings/SettingsScreen.tsx
+++ screens/Settings/SettingsScreen.tsx
@@ -1,16 +1,26 @@
-import { Text } from "react-native";
+import { StyleSheet, Text } from "react-native";
 import MySafeAreaView from "../../shared/MySafeAreaView";
+import { typography } from "../../theme";
 
 interface SettingsScreenProps {}
 
 const SettingsScreen: React.FC<SettingsScreenProps> = () => {
   return (
     <>
-      <MySafeAreaView>
-        <Text>Settings</Text>
+      <MySafeAreaView style={styles.container}>
+        <Text style={styles.title}>Settings</Text>
       </MySafeAreaView>
     </>
   );
 };
 
 export default SettingsScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    marginLeft: 15,
+  },
+  title: {
+    ...typography.title1,
+  },
+});
