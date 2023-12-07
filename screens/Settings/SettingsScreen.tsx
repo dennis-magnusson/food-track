210b6bf5e307cfbda4ef4b2c0118b5cff45ee6fd
Dennis Magnusson <magnden@gmail.com>
2023-12-07 22:53:51 +0200

    add content to settings screen

--- screens/Settings/SettingsScreen.tsx
+++ screens/Settings/SettingsScreen.tsx
@@ -1,26 +1,57 @@
-import { StyleSheet, Text } from "react-native";
+import React, { useState } from "react";
+import { Button, ScrollView, StyleSheet, Text, View } from "react-native";
 import MySafeAreaView from "../../shared/MySafeAreaView";
-import { typography } from "../../theme";
+import { colors, layout, typography } from "../../theme";
 
-interface SettingsScreenProps {}
+const SettingsScreen: React.FC = () => {
+  const [theme, setTheme] = useState("auto");
 
-const SettingsScreen: React.FC<SettingsScreenProps> = () => {
+  const handleThemeChange = (newTheme: string) => {
+    setTheme(newTheme);
+  };
+
+  const handleExportData = () => {
+    console.log("Data export"); // TODO: implement
+  };
+
   return (
-    <>
-      <MySafeAreaView style={styles.container}>
-        <Text style={styles.title}>Settings</Text>
-      </MySafeAreaView>
-    </>
+    <MySafeAreaView style={styles.container}>
+      <Text style={styles.title}>Settings</Text>
+      <ScrollView bounces={true} style={styles.scrollView}>
+        <View style={styles.buttonContainer}>
+          <Button
+            title="About"
+            style={styles.button}
+            onPress={() => console.log("About pressed")}
+          />
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button
+            title="Export Data"
+            style={styles.button}
+            onPress={handleExportData}
+          />
+        </View>
+      </ScrollView>
+    </MySafeAreaView>
   );
 };
 
-export default SettingsScreen;
-
 const styles = StyleSheet.create({
   container: {
-    marginLeft: 15,
+    flex: 1,
+    backgroundColor: colors.lightBackground,
   },
   title: {
     ...typography.title1,
+    marginLeft: 15,
+  },
+  scrollView: {
+    marginTop: layout.accentContainer1.margin,
+  },
+  buttonContainer: {
+    ...layout.accentContainer1,
   },
 });
+
+export default SettingsScreen;
