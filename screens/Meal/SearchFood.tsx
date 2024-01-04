a5be47a4158e09052136f5508af3be5172d4e0a3
dennis <magnden@gmail.com>
2024-01-04 14:21:25 +0200

    add barcode screen button to meal screen

--- screens/Meal/SearchFood.tsx
+++ screens/Meal/SearchFood.tsx
@@ -1,11 +1,14 @@
 import React from "react";
-import { StyleSheet } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { StyleSheet, View } from "react-native";
 import MyButton from "../../shared/MyButton";
 import { Food } from "../../types";
 import SearchFoodsList from "./SearchFoodsList";
+import { typography } from "../../theme";
 
 interface SearchFoodProps {
   handleAddCustomFood: () => void;
+  handleScanBarcode: () => void;
   handleFoodPress: (food: Food) => void;
   filteredFoods: Food[];
 }
@@ -14,14 +17,25 @@ const SearchFood: React.FC<SearchFoodProps> = ({
   handleAddCustomFood,
   filteredFoods,
   handleFoodPress,
+  handleScanBarcode
 }): JSX.Element => {
   return (
     <>
+    <View style={styles.buttonContainer}>
+    <MyButton
+        text="Scan Barcode"
+        style={styles.scanButton}
+        onPress={handleScanBarcode}
+        icon={<Ionicons name="barcode-outline" size={24} color="white" />}
+      />
       <MyButton
-        text="Add Custom Food"
+        text="Manual Entry"
         style={styles.addButton}
         onPress={handleAddCustomFood}
+        icon={<Ionicons name="add-outline" size={24} color="white" />}
       />
+    </View>
+      
       <SearchFoodsList
         foods={filteredFoods}
         handleFoodPress={handleFoodPress}
@@ -31,8 +45,22 @@ const SearchFood: React.FC<SearchFoodProps> = ({
 };
 
 const styles = StyleSheet.create({
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "stretch",
+    gap: 10,
+  },
   addButton: {
     marginHorizontal: 10,
+    flex: 1,
+    marginLeft: 0
+  },
+  scanButton: {
+    marginHorizontal: 10,
+    flex: 1,
+    backgroundColor: "#ff9900",
+    marginRight: 0
   },
 });
 
