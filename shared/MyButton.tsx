b1f5908b491001418ae12939682470bd3a3395cb
dennis <magnden@gmail.com>
2024-01-04 14:20:54 +0200

    add optional icon to my button

--- shared/MyButton.tsx
+++ shared/MyButton.tsx
@@ -3,6 +3,7 @@ import {
   StyleSheet,
   TouchableOpacity,
   ViewStyle,
+  View
 } from "react-native";
 import { colors } from "../theme";
 import { MyText } from "./MyText";
@@ -11,29 +12,37 @@ interface MyButtonProps {
   onPress: () => void;
   text: string;
   style?: StyleProp<ViewStyle>;
+  icon?: JSX.Element;
 }
 
-const MyButton = ({ onPress, text, style, ...rest }: MyButtonProps) => {
-  const containerStyles = [styles.addButton, style];
+const MyButton = ({ onPress, text, style, icon, ...rest }: MyButtonProps) => {
+  const containerStyles = [styles.buttonBase, style];
   return (
     <TouchableOpacity onPress={onPress} style={containerStyles} {...rest}>
-      <MyText style={styles.addButtonLabel}>{text}</MyText>
+      <View style={styles.container}>{icon}<MyText style={styles.buttonLabel}>{text}</MyText></View>
     </TouchableOpacity>
   );
 };
 
 const styles = StyleSheet.create({
-  addButton: {
+  buttonBase: {
     backgroundColor: colors.accent,
     padding: 20,
     marginBottom: 10,
     alignItems: "center",
     borderRadius: 10,
   },
-  addButtonLabel: {
+  buttonLabel: {
     color: colors.lightText,
     fontWeight: "bold",
   },
+  container: {
+    flexDirection: "row",
+    alignContent: "center",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 5
+  }
 });
 
 export default MyButton;
