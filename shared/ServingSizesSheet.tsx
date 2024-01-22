e9eaf98af763292ec6d2fe57e27219f5c7dcd3c0
Dennis Magnusson <magnden@gmail.com>
2024-01-22 09:53:21 +0200

    move serving sizes selection sheet into its own component

---
+++ shared/ServingSizesSheet.tsx
@@ -0,0 +1,72 @@
+import { Dispatch, SetStateAction } from "react";
+import { ScrollView, TouchableOpacity } from "react-native";
+import RBSheet from "react-native-raw-bottom-sheet";
+import { CUSTOM_SERVING_SIZE_OPTION } from "../constants/customFood";
+import { inputs, typography } from "../theme";
+import { CustomServingSize, Food, ServingSize } from "../types";
+import { MyText } from "./MyText";
+import NutritionFactsTable from "./NutritionFactsTable";
+
+interface ServingSizesSheetProps {
+  sheetRef: React.RefObject<RBSheet>;
+  servingSizes: ServingSize[];
+  setServingSize: Dispatch<SetStateAction<ServingSize | CustomServingSize>>;
+  food: Food;
+  currentlySelectedAmount: number;
+}
+
+const ServingSizesSheet: React.FC<ServingSizesSheetProps> = ({
+  sheetRef,
+  servingSizes,
+  setServingSize,
+  food,
+  currentlySelectedAmount,
+}) => {
+  return (
+    <>
+      <RBSheet
+        ref={sheetRef}
+        closeOnDragDown={true}
+        height={300}
+        customStyles={{
+          wrapper: {
+            backgroundColor: "transparent",
+          },
+        }}
+      >
+        <ScrollView style={{ margin: 10, paddingBottom: 100 }} bounces={false}>
+          <MyText style={{ ...typography.title2, marginBottom: 10 }}>
+            Select serving size
+          </MyText>
+          {servingSizes.map((servingSize) => (
+            <TouchableOpacity
+              key={servingSize.id}
+              onPress={() => {
+                setServingSize(servingSize);
+                sheetRef.current?.close();
+              }}
+              style={{ marginBottom: 10, paddingVertical: 5 }}
+            >
+              <MyText
+                style={inputs.textInput}
+              >{`${servingSize.description} (${servingSize.amount}${food.per100unit})`}</MyText>
+            </TouchableOpacity>
+          ))}
+          <TouchableOpacity
+            onPress={() => {
+              setServingSize(CUSTOM_SERVING_SIZE_OPTION);
+              sheetRef.current?.close();
+            }}
+            style={{ marginBottom: 10, paddingVertical: 5 }}
+          >
+            <MyText style={inputs.textInput}>Custom amount (1g)</MyText>
+          </TouchableOpacity>
+        </ScrollView>
+      </RBSheet>
+
+      <NutritionFactsTable food={food} amount={currentlySelectedAmount} />
+    </>
+  );
+};
+
+export default ServingSizesSheet;
