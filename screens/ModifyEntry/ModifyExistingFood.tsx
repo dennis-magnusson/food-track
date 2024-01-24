6afde0201428ab012902b182f42ead0f9036ffbb
Dennis Magnusson <magnden@gmail.com>
2024-01-24 15:32:28 +0200

    improve modify existing food screen to work with serving sizes

--- screens/ModifyEntry/ModifyExistingFood.tsx
+++ screens/ModifyEntry/ModifyExistingFood.tsx
@@ -1,24 +1,31 @@
 import { RouteProp, useNavigation } from "@react-navigation/native";
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import {
   KeyboardAvoidingView,
   StyleSheet,
   TextInput,
+  TouchableOpacity,
   View,
 } from "react-native";
-import { DayDispatchContext } from "../../context/AppContext";
+import RBSheet from "react-native-raw-bottom-sheet";
+import { CUSTOM_SERVING_SIZE_OPTION } from "../../constants/customFood";
+import { DayContext, DayDispatchContext } from "../../context/AppContext";
 import {
   deleteFoodEntry,
+  fetchServingSizesForFood,
   updateAmountToFoodEntry,
 } from "../../services/databaseService";
 import BackButton from "../../shared/BackButton";
 import MyButton from "../../shared/MyButton";
 import MySafeAreaView from "../../shared/MySafeAreaView";
 import { MyText } from "../../shared/MyText";
+import ServingSizesSheet from "../../shared/ServingSizesSheet";
 import { colors, inputs, typography } from "../../theme";
 import {
+  FoodEntryBeforeInsert,
   ModifyFoodEntryScreenNavigationProp,
   RootStackParamList,
+  ServingSize,
 } from "../../types";
 
 interface ModifyFoodEntryScreenProps {
@@ -28,28 +35,85 @@ interface ModifyFoodEntryScreenProps {
 const ModifyFoodEntryScreen: React.FC<ModifyFoodEntryScreenProps> = ({
   route,
 }) => {
-  const { entry, mealType } = route.params;
-  console.log(entry, mealType);
-  const [servingSize, setServingSize] = useState<string>(
-    entry.amount.toString()
-  );
+  const [nServings, setNServings] = useState<string>("1");
+  const [servingSize, setServingSize] = useState<ServingSize>(null);
+  const [servingSizes, setServingSizes] = useState<ServingSize[]>([]);
 
   const navigation = useNavigation<ModifyFoodEntryScreenNavigationProp>();
   const dispatch = useContext(DayDispatchContext);
+  const day = useContext(DayContext);
+
+  const { entry, mealType, mealId } = route.params;
+  const food = entry.food;
+
+  const servingSizesSheetRef = useRef<RBSheet>();
+
+  useEffect(() => {
+    const fetchServingSizes = async () => {
+      const fetchedServingSizes = await fetchServingSizesForFood(food.id);
+      setServingSizes([...servingSizes, ...fetchedServingSizes]);
+    };
+    fetchServingSizes();
+  }, []);
+
+  useEffect(() => {
+    if ("nServings" in entry && "servingSize_id" in entry) {
+      setNServings(entry.nServings.toString());
+      if (servingSizes.length > 0) {
+        setServingSize(servingSizes.find((s) => s.id === entry.servingSize_id));
+      }
+    } else if ("customAmount" in entry) {
+      setNServings(entry.customAmount.toString());
+    } else {
+      throw new Error("Invalid food entry");
+    }
+  }, [entry, servingSizes]);
 
   const handleModifyFoodEntry = async () => {
-    const newAmount = parseFloat(servingSize) || 100;
+    if (nServings === "") {
+      alert("Please enter a valid amount");
+      return;
+    }
+
+    let foodEntry: FoodEntryBeforeInsert;
+
+    if (servingSize === null) {
+      foodEntry = {
+        food: food,
+        customAmount: getNServingsInputValueParsed(),
+      };
+    } else {
+      foodEntry = {
+        food: food,
+        servingSize_id: servingSize.id,
+        nServings: getNServingsInputValueParsed(),
+      };
+    }
+
+    console.log("modified food entry: ", foodEntry);
 
     try {
-      await updateAmountToFoodEntry(entry.id, newAmount);
-      dispatch({
-        type: "CHANGE_FOOD_AMOUNT",
-        payload: {
-          mealType,
-          entryId: entry.id,
-          newAmount,
-        },
-      });
+      await updateAmountToFoodEntry(entry.id, foodEntry);
+      if ("customAmount" in foodEntry) {
+        dispatch({
+          type: "CHANGE_FOOD_AMOUNT",
+          payload: {
+            mealType,
+            entryId: entry.id,
+            customAmount: foodEntry.customAmount,
+          },
+        });
+      } else {
+        dispatch({
+          type: "CHANGE_FOOD_AMOUNT",
+          payload: {
+            mealType,
+            entryId: entry.id,
+            servingSize_id: foodEntry.servingSize_id,
+            nServings: foodEntry.nServings,
+          },
+        });
+      }
       navigation.navigate("Meal", { mealType });
     } catch (error) {
       alert(error);
@@ -72,16 +136,17 @@ const ModifyFoodEntryScreen: React.FC<ModifyFoodEntryScreenProps> = ({
     }
   };
 
+  const getNServingsInputValueParsed = (): number =>
+    parseFloat(nServings.replace(",", "."));
+
   const handleInputChange = (text: string) => {
     if (
       (text === "" || /^[\d]*[.,]?[\d]*$/.test(text)) &&
       !/^[.,]$/.test(text)
     ) {
-      setServingSize(text);
+      setNServings(text);
     }
   };
-
-  console.log();
 
   return (
     <MySafeAreaView>
@@ -91,15 +156,30 @@ const ModifyFoodEntryScreen: React.FC<ModifyFoodEntryScreenProps> = ({
           <MyText style={styles.title}>{entry.food.name}</MyText>
           <View style={styles.inputContainer}>
             <TextInput
-              autoFocus={true}
               style={styles.input}
-              placeholder="Amount"
               keyboardType="numeric"
               onChangeText={handleInputChange}
-              value={servingSize}
+              value={nServings}
+              autoFocus={true}
             />
-            <MyText style={styles.inputUnits}>{entry.food.per100unit}</MyText>
+            <TouchableOpacity
+              onPress={() => servingSizesSheetRef.current?.open()}
+              style={styles.selectorButton}
+            >
+              {servingSize === null ? (
+                <MyText>{`${CUSTOM_SERVING_SIZE_OPTION.description} (${CUSTOM_SERVING_SIZE_OPTION.amount}${food.per100unit})`}</MyText>
+              ) : (
+                <MyText>{`${servingSize.description} (${servingSize.amount}${food.per100unit})`}</MyText>
+              )}
+            </TouchableOpacity>
           </View>
+          <ServingSizesSheet
+            servingSizes={servingSizes}
+            setServingSize={setServingSize}
+            food={entry.food}
+            currentlySelectedAmount={getNServingsInputValueParsed()}
+            sheetRef={servingSizesSheetRef}
+          />
           <View
             style={{
               flexDirection: "row",
@@ -142,7 +222,12 @@ const styles = StyleSheet.create({
   },
   input: {
     ...inputs.textInput,
-    flex: 0.5,
+    marginRight: 10,
+    flex: 0.1,
+  },
+  selectorButton: {
+    flex: 0.9,
+    ...inputs.textInput,
   },
 });
 
