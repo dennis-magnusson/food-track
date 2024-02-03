a28d665eb9e9cd8210a21b3c0789f9195a098208
Dennis Magnusson <magnden@gmail.com>
2024-02-03 22:06:49 +0200

    implement dummy settings for now

--- screens/Settings/SettingsScreen.tsx
+++ screens/Settings/SettingsScreen.tsx
@@ -1,11 +1,17 @@
+import { useNavigation } from "@react-navigation/native";
 import React, { useState } from "react";
-import { Button, ScrollView, StyleSheet, View } from "react-native";
+import { ScrollView, StyleSheet } from "react-native";
+import BackButton from "../../shared/BackButton";
 import MySafeAreaView from "../../shared/MySafeAreaView";
 import { MyText } from "../../shared/MyText";
 import { layout, typography } from "../../theme";
+import SectionDivider from "./SectionDivider";
+import SettingItem from "./SettingItem";
 
 const SettingsScreen: React.FC = () => {
   const [theme, setTheme] = useState("auto");
+
+  const navigation = useNavigation();
 
   const handleThemeChange = (newTheme: string) => {
     setTheme(newTheme);
@@ -17,14 +23,21 @@ const SettingsScreen: React.FC = () => {
 
   return (
     <MySafeAreaView style={styles.container}>
+      <BackButton backFunction={navigation.goBack} />
       <MyText style={styles.title}>Settings</MyText>
       <ScrollView bounces={true} style={styles.scrollView}>
-        <View style={styles.buttonContainer}>
-          <Button title="About" onPress={() => console.log("About pressed")} />
-        </View>
-        <View style={styles.buttonContainer}>
-          <Button title="Export Data" onPress={handleExportData} />
-        </View>
+        <SectionDivider sectionName="Appearance" />
+        <SettingItem itemName="Theme" onPress={() => {}} value="Auto" />
+        <SettingItem itemName="Language" onPress={() => {}} value="English" />
+        <SettingItem
+          itemName="First day of the week"
+          onPress={() => {}}
+          value="Mon"
+        />
+        <SectionDivider sectionName="Data" />
+        <SettingItem itemName="Export data" onPress={handleExportData} />
+        <SettingItem itemName="Import data" onPress={() => {}} />
+        <SettingItem itemName="Clear data" onPress={() => {}} />
       </ScrollView>
     </MySafeAreaView>
   );
@@ -40,9 +53,6 @@ const styles = StyleSheet.create({
   },
   scrollView: {
     marginTop: layout.accentContainer1.margin,
-  },
-  buttonContainer: {
-    ...layout.accentContainer1,
   },
 });
 
